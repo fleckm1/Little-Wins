43238e83f691455a290289fcad80a5b8a29df42f
fleckm1 <fleck.missy@gmail.com>
2026-09-07 21:25:01 -0500

    Upgrade service worker to version 2
    Updated service worker to version 2 with improved cache management and fetch handling.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,51 @@
-const CACHE='little-wins-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE='little-wins-v2';
+const ASSETS=[
+'./',
+'./index.html',
+'./manifest.webmanifest',
+'./icon-192.png',
+'./icon-512.png'
+];
+
+self.addEventListener('install', e => {
+e.waitUntil(
+caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+);
+self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+e.waitUntil(
+Promise.all([
+caches.keys().then(keys =>
+Promise.all(
+keys
+.filter(key => key !== CACHE)
+.map(key => caches.delete(key))
+)
+),
+self.clients.claim()
+])
+);
+});
+
+self.addEventListener('fetch', e => {
+if (e.request.mode === 'navigate') {
+e.respondWith(
+fetch(e.request)
+.then(response => {
+const copy = response.clone();
+caches.open(CACHE).then(cache => {
+cache.put('./index.html', copy);
+});
+return response;
+})
+.catch(() => caches.match('./index.html'))
+);
+return;
+}
+
+e.respondWith(
+caches.match(e.request).then(response => response || fetch(e.request))
+);
+});
