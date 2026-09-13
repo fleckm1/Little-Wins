e47ec39a742de6f32b11c1eb2aa6a8f4bc03879c
fleckm1 <fleck.missy@gmail.com>
2026-09-13 18:07:39 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,18 @@
-const CACHE='little-wins-v3';
-
-const ASSETS=[
-'./',
-'./index.html',
-'./manifest.webmanifest',
-'./icon-192.png',
-'./icon-512.png'
-];
-
-self.addEventListener('install', e => {
-e.waitUntil(
-caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-);
-self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-e.waitUntil(
-Promise.all([
-caches.keys().then(keys =>
-Promise.all(
-keys
-.filter(key => key !== CACHE)
-.map(key => caches.delete(key))
-)
-),
-self.clients.claim()
-])
-);
-});
-
-self.addEventListener('fetch', e => {
-if (e.request.mode === 'navigate') {
-e.respondWith(
-fetch(e.request)
-.then(response => {
-const copy = response.clone();
-
-caches.open(CACHE).then(cache => {
-cache.put('./index.html', copy);
-});
-
-return response;
-})
-.catch(() => caches.match('./index.html'))
-);
-
-return;
-}
-
-e.respondWith(
-caches.match(e.request).then(response =>
-response || fetch(e.request)
-)
-);
+const CACHE='little-wins-v7';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./little-wins-sunrise.jpg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([
+ caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+ self.clients.claim()
+]));});
+self.addEventListener('fetch',e=>{
+ if(e.request.mode==='navigate'){
+  e.respondWith(fetch(e.request).then(r=>{
+   const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;
+  }).catch(()=>caches.match('./index.html')));
+  return;
+ }
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+  const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;
+ })));
 });
