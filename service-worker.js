68c7f55afa1bbdfbe7634fe6d06c072d48197c60
fleckm1 <fleck.missy@gmail.com>
2026-09-13 18:23:54 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,16 @@
-const CACHE='little-wins-v7-1';
+const CACHE='little-wins-v7-2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./little-wins-sunrise.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]));});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([
+ caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+ self.clients.claim()
+]));});
 self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;}).catch(()=>caches.match('./index.html')));return;}
+ if(e.request.mode==='navigate'){
+  e.respondWith(fetch(e.request).then(r=>{
+   const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;
+  }).catch(()=>caches.match('./index.html')));
+  return;
+ }
  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
