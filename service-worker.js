daee87f8dedc6aecc11b188cf419d0342eccb3d2
fleckm1 <fleck.missy@gmail.com>
2026-09-13 18:44:21 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='little-wins-v7-2-1';
+const CACHE='little-wins-v7-2-2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./little-wins-sunrise.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([
@@ -7,8 +7,10 @@ self.addEventListener('activate',e=>{e.waitUntil(Promise.all([
 ]));});
 self.addEventListener('fetch',e=>{
  if(e.request.mode==='navigate'){
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;})
-  .catch(()=>caches.match('./index.html')));return;
+  e.respondWith(fetch(e.request).then(r=>{
+   const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;
+  }).catch(()=>caches.match('./index.html')));
+  return;
  }
  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
