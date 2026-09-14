12b3b763cc1d64177fb37482d3413e36652023d0
fleckm1 <fleck.missy@gmail.com>
2026-09-13 19:37:30 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='little-wins-v7-2-8';
+const CACHE='little-wins-v7-2-9';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./little-wins-sunrise.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([
